6be310d040f6b649b3f43447791a2412960c1bdc
maartenvandenbrande <maarten.vdbrande@gmail.com>
2022-10-24 14:52:17 +0200

    implementation of nested loop join

---
+++ src/innerJoinAlgorithms/nestedLoopJoin.ts
@@ -0,0 +1,22 @@
+import {Relation} from "../relation/relation";
+
+export function nestedLoopJoin(left: Relation, right: Relation, a: string) {
+  const output = new Relation();
+  left.forEach((leftElement) => {
+    right.forEach((rightElement) => {
+      if (leftElement[a] == rightElement[a]) {
+        let obj: Record<string, any> = {};
+        for (const [key, value] of Object.entries(leftElement)) {
+          obj[key] = value;
+        }
+        for (const [key, value] of Object.entries(rightElement)) {
+          if (key !== a) {
+            obj[key] = value;
+          }
+        }
+        output.add(obj);
+      }
+    });
+  });
+  return output;
+}
